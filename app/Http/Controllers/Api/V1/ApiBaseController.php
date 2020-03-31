ab48c2dbd360b9465851f528e11d839647c681e8
tintaungkhant <seasoft.tint.khant@gmail.com>
2020-03-31 10:34:03 +0630

    pure larave and api base controller

---
+++ app/Http/Controllers/Api/V1/ApiBaseController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApiBaseController extends Controller
+{
+    //
+    /**
+     * @param int $http_status
+     * @param int $status
+     * @param array $headers
+     * @param array $message
+     * @param array $data
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function jsonResponse($http_status=200, $status=1, $headers=[], $message=[], $data=[]){
+        return response()->json([
+            "status" => $status,
+            "message" => $message,
+            "headers" => $headers,
+            "data" => $data
+        ],$http_status);
+    }
+}
