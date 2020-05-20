22162febfa45b532b5f3a064ae946ed0c2548b59
Wai Yan Htet <whispx93@gmail.com>
2020-05-20 17:03:55 +0630

    remove admin panel code

--- config/app.php
+++ config/app.php
@@ -163,7 +163,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Maatwebsite\Excel\ExcelServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -222,6 +221,5 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     ],
 ];
