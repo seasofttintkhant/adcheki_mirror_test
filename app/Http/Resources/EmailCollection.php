26a0236d933a8622025c9038d9ce652fa446a427
Wai Yan Htet <whispx93@gmail.com>
2020-06-10 10:36:10 +0630

    change inproces

--- app/Http/Resources/EmailCollection.php
+++ app/Http/Resources/EmailCollection.php
@@ -17,7 +17,7 @@ class EmailCollection extends ResourceCollection
         return [
             'device_id' => $request->device_id,
             'created_at' => $this->collection[0]->device->updated_at->format('Y-m-d H:i:s'),
-            'in_process' => !!$this->collection[0]->device->is_checked,
+            'in_process' => !$this->collection[0]->device->is_checked,
             'result' => EmailResource::collection($this->collection)
         ];
     }
