17df9056e4e5b557603da6e925f62a6460f037da
DESKTOP-NFECR6C\koworski <sesoft.tint.khant@gmail.com>
2020-09-30 17:38:23 +0630

    Remove static url from job.

--- app/Jobs/VerifyEmailJob.php
+++ app/Jobs/VerifyEmailJob.php
@@ -156,7 +156,7 @@ class VerifyEmailJob implements ShouldQueue
         $mail_checking_servers = env('MAIL_CHECKING_SERVERS', '');
         $mail_checking_servers = explode(',', $mail_checking_servers);
         $mail_checking_server = $mail_checking_servers[array_rand($mail_checking_servers)];
-        $mail_checking_server = 'https://check01.adcheki.jp';
+        // $mail_checking_server = 'https://check01.adcheki.jp';
         $payload = [
             'emails' => $emails,
             'job_id' => $this->job->getJobId(),
